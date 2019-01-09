59bda59f929c71c11b67e017a4abb91ab55b9a1d
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-09 16:35:41 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def turn_count(board)
+  
+  counter= 0
+   board.each do |space|
+    if space == "X" || space == "O"
+  counter += 1
+  puts "#{counter}"
+  
+end
+end
+counter
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    puts "X"
+  else
+    puts "O"
+  end
+end
